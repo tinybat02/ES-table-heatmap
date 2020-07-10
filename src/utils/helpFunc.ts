532a66ec50ffbe7c28c71bb62cb1505a21a766a8
Nam Le <nam.leduc02@gmail.com>
2020-07-10 12:32:29 +0200

    clg

--- src/utils/helpFunc.ts
+++ src/utils/helpFunc.ts
@@ -26,6 +26,8 @@ export const processData = (valueArr: number[], timestampArr: number[]) => {
   });
 
   const numberOfWeeks = Object.keys(totalWeeks).length;
+  console.log('weeks ', totalWeeks);
+  console.log('template ', templateTable);
   for (let i = 0; i < 6; i++) {
     hours.map(hour => {
       if (templateTable[i][hour] == 0) {
